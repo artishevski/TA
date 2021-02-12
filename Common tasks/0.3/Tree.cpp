5bb6759583d7332ec5afb78047f81f630ede390b
Aliaksandr Artsisheuski <artsisheuski24@gmail.com>
2021-02-12 21:21:11 +0300

    added 0.3

---
+++ Common tasks/0.3/Tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 12-Feb-21.
+//
+
+#include "Tree.h"
---
+++ Common tasks/0.3/Tree.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 12-Feb-21.
+//
+
+#ifndef INC_0_3_TREE_H
+#define INC_0_3_TREE_H
+
+
+class Tree {
+
+};
+
+
+#endif //INC_0_3_TREE_H
